a20b11adf4ceac0b0597567392ec190fd5ff0dc4
wpsaputra <wayanps93@gmail.com>
2022-10-14 13:53:05 +0800

    fix idcard retrieve from onedataasn

--- src/pages/IdCard.jsx
+++ src/pages/IdCard.jsx
@@ -6,18 +6,44 @@ export default function IdCard({match, location}) {
     const [state, setState] = useState({
         isLoading: true,
         pegawai: {},
-        isNotFound: false
+        isNotFound: false,
+        jabatan: {},
+        tmt: "",
+        nm_satker:"",
+        alamat: "",
+        pangkat: "",
+        nm_golongan: "",
     });
 
     const loadPegawaiDetail = () => {
         // setState({ count: state.count + 1 });
         // axios.get(url_api+'/records/anime/'+match.params.animeId+'?join=genre')
         // axios.get(url_api+'/records/anime?filter=link,eq,'+match.params.animeId+'&join=genre&join=rating&join=source&join=status&join=studio&join=type')
-        axios.get('https://webapps.bps.go.id/sultra/idcard/api.php/records/idcard/'+match.params.niplama+'?join=instansi')
+        
+        // axios.get('https://webapps.bps.go.id/sultra/idcard/api.php/records/idcard/'+match.params.niplama+'?join=instansi')
+        axios.get(`https://sultradata.com/project/idcard-api/api.php/records/users?filter=niplama,eq,${match.params.niplama}&join=users_jabatan,master_satker&join=users_golongan,master_golongan`)
             .then(function (response) {
                 // handle success
-                console.log(response.data);
-                setState({ ...state, pegawai: response.data, isLoading: false });
+                console.log(response.data.records[0]);
+                let jabatan = response.data.records[0].users_jabatan[response.data.records[0].users_jabatan.length-1];
+                let tmt = jabatan.tmt.split("-");
+                tmt = tmt[2]+"-"+tmt[1]+"-"+tmt[0];
+                let nm_satker = jabatan.id_satker.nm_satker;
+                let alamat = jabatan.id_satker.alamat;
+
+                let golongan = response.data.records[0].users_golongan[response.data.records[0].users_golongan.length-1].id_golongan;
+                let pangkat = golongan.pangkat;
+                let nm_golongan = golongan.nm_golongan;
+
+                console.log("golongan", golongan);
+                console.log("pangkat", pangkat);
+                console.log("nm_golongan", nm_golongan);
+
+
+                setState({ ...state, pegawai: response.data.records[0], isLoading: false,  jabatan: jabatan, tmt: tmt, nm_satker: nm_satker, alamat: alamat, 
+                    pangkat: pangkat, 
+                    nm_golongan: nm_golongan
+                });
                 // console.log(response.data.records[0]);
                 // console.log(response.data.records.length);
                 // if (typeof response.data.records[0] === 'undefined') {
@@ -69,32 +95,39 @@ export default function IdCard({match, location}) {
                             <div className="personal_text">
                                 <h6>Hello Everybody, i am</h6>
                                 {/* <h3>Moh Edy Mahmud, S.Si, M.P</h3> */}
-                                <h3>{state.pegawai.nama+", "+state.pegawai.gelar_belakang}</h3>
+                                <h3>{state.pegawai.nama_lengkap+", "+state.pegawai.gelar_belakang}</h3>
                                 {/* <h4>Kepala BPS Provinsi Sulawesi Tenggara</h4> */}
-                                <h4>{state.pegawai.jabatan}</h4>
+                                <h4>{state.jabatan.nm_jabatan} (TMT {state.tmt})</h4>
+                                {/* <h4>{state.pegawai.users_jabatan}</h4> */}
                                 <br/>
                                 <ul className="list basic_info">
                                     {/* <li><a href="#"><i className="lnr lnr-calendar-full"></i> 196902151991121001</a></li> */}
                                     <li><a href="#"><i className="lnr lnr-calendar-full"></i> {state.pegawai.nipbaru}</a></li>
                                     {/* <li><a href="#"><i className="lnr lnr-license"></i> IV/c</a></li> */}
-                                    <li><a href="#"><i className="lnr lnr-license"></i> {state.pegawai.pangkat}</a></li>
+                                    {/* <li><a href="#"><i className="lnr lnr-license"></i> {state.pegawai.pangkat}</a></li> */}
+                                    <li><a href="#"><i className="lnr lnr-license"></i> {state.pangkat+" ("+state.nm_golongan+")"}</a></li>
                                     {/* <li><a href="#"><i className="lnr lnr-phone-handset"></i> +62 813-5128-9800</a></li> */}
                                     {/* <li><a href="#"><i className="lnr lnr-envelope"></i>edimahmud@bps.go.id</a></li> */}
                                     <li><a href="#"><i className="lnr lnr-envelope"></i>{state.pegawai.email}</a></li>
                                     {/* <li><a href="#"><i className="lnr lnr-home"></i> BPS Provinsi Sulawesi Tenggara, </a></li> */}
                                     {/* <li style={{marginTop: "-20px"}}><a href="#"><i className="lnr lnr-home" style={{visibility: "hidden"}}></i> Jl. Boulevard No 1 Mokoau, Kambu, Kendari, Sulawesi Tenggara </a></li> */}
                                     {/* <li><a href="#"><i className="lnr lnr-home"></i> {state.pegawai.id_satker.instansi+","} </a></li> */}
-                                    {typeof state.pegawai.id_satker!=='undefined' ? (
+                                    
+                                    {/* {typeof state.pegawai.id_satker!=='undefined' ? (
                                         <li><a href="#"><i className="lnr lnr-home"></i> {state.pegawai.id_satker.instansi} </a></li>
                                     ) : (
                                         <></>
-                                    )}
-                                    {typeof state.pegawai.id_satker!=='undefined' ? (
+                                    )} */}
+                                    <li><a href="#"><i className="lnr lnr-home"></i> {state.nm_satker} </a></li>
+
+
+                                    {/* {typeof state.pegawai.id_satker!=='undefined' ? (
                                         // <li><a href="#"><i className="lnr lnr-home"></i> {state.pegawai.id_satker.instansi} </a></li>
                                         <li style={{marginTop: "-20px"}}><a href="#" style={{display: "inline-block"}}><i className="lnr lnr-home" style={{visibility: "hidden"}}></i> {state.pegawai.id_satker.alamat} </a></li>
                                     ) : (
                                         <></>
-                                    )}
+                                    )} */}
+                                    <li style={{marginTop: "-20px"}}><a href="#" style={{display: "inline-block"}}><i className="lnr lnr-home" style={{visibility: "hidden"}}></i> {state.alamat} </a></li>
                                 </ul>
                                 {state.isNotFound ? (
                                     // <Redirect to={{pathname: '/404'}} />

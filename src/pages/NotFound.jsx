0a79e3c545b4a1e8f87f88ee948ce80b4790cdcd
wpsaputra <wayanps93@gmail.com>
2022-10-14 14:04:23 +0800

    fix idcard

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import "../custom.css"
 
 export default function NotFound() {
     return (
-        <div>
-            <h1 style={{color: "black"}}>404 Not Found</h1>
+        <div id="main">
+            <div className="fof">
+                <h1>Error 404</h1>
+                <p>Pastikan niplama url sudah sesuai ex: <a href="http://webapps.bps.go.id/sultra/idcard/340057236">http://webapps.bps.go.id/sultra/idcard/340057236</a> </p>
+            </div>
         </div>
+
     )
 }
